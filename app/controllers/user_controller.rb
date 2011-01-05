da1b9db599187b9018a7c5720109717503e0fc23
Merritt Boyd <mboyd@mit.edu>
2011-01-05 13:27:20 -0500

    Stubbed user controller and views.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  def create
+  end
+
+  def display
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
---
+++ test/functional/user_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UserControllerTest < ActionController::TestCase
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get display" do
+    get :display
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
+  end
+
+end
