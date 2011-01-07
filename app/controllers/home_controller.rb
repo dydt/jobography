26dc3a17fbc925743e70ee9f148dc084f77d09ee
Merritt Boyd <mboyd@mit.edu>
2011-01-06 21:02:37 -0500

    Basic map view for the homepage.
    This will need major refactoring later, but it's a cool demo for now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+
   def welcome
+    @rand_jobs = []
+    10.times do
+      i = rand(Job.count)
+      @rand_jobs.push(Job.first(:offset => i))
+    end
   end
 
 end
