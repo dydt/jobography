e5f65a76eb18f00797aa2f228f46fac4abc6214d
Merritt Boyd <mboyd@mit.edu>
2011-01-09 22:01:26 -0500

    Email handler for user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       redirect_to users_url
     else
       render :new
