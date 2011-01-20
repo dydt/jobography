85f35e394a443e1e2fa9c90aa56f661e35b613c6
Diyang <dydt@mit.edu>
2011-01-20 01:49:26 -0500

    Scrapes FB contact work info!

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,16 +31,63 @@ class User < ActiveRecord::Base
     end
   end
 
-  def import_fb_contacts  
-    path = "/#{self.facebook_id}?access_token=#{self.facebook_access_token}"
+
+  def import_user_fb  
+    token = "access_token=#{self.facebook_access_token}"
+
+    user_path = "/#{self.facebook_id}?" + token
+    
+    user_resp = facebook_call(user_path)
+    
+    if user_resp.has_key?("work")
+      return user_resp["work"].map do |e|
+        f = Employment.new
+        f.employer = e["employer"]["name"] if e['employer']
+        f.location = e['location']["name"] if e['location']
+        f.title = e["position"]["name"] if e['position']
+        f.start_date = e["start date"]
+        f.end_date = e["end_date"]
+
+        f
+      end     
+    end
+  
+    get_friend_path = "/#{self.facebook_id}/friends?" + token
+
+    friends_list = facebook_call(get_friend_path)
+    friends_ids = friends_list["data"].map {|f| f["id"]}
+    friends_paths = friends_ids.map {|id| "/#{id}?" + token}
+    friend_resp = friends_paths.map do |p| 
+      facebook_call(p)
+    end
+    friend_resp = friend_resp.each.select {|h| h.has_key?("work")}
+    friend_resp.map do |fs|
+      fs["work"].map do |f|
+        e = Employment.new
+        e.employer = f["employer"]["name"] if f["employer"]
+        e.location = f["location"]["name"] if f["location"]
+        e.title = f["position"]["name"] if f["position"]
+        e.start_date = f["start date"]
+        e.end_date = f["end_date"]
+    
+        e
+      end
+    end
+  end
+
+  def facebook_call(path)
+    puts path
     http = Net::HTTP.new('graph.facebook.com', 443)
     http.use_ssl = true
 
     resp = http.get(path)
-    respText = resp.body
+    resp_text = resp.body
     
-  end
-    
+    jsonresp = JSON.parse(resp_text)
+    return [] unless jsonresp
+
+    jsonresp
+  end  
   
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     User.find_by_facebook_id(access_token['uid'])
