c1f8cec5446ae512ca70b3dc06e3f46da7a270cc
Merritt Boyd <mboyd@mit.edu>
2011-01-25 02:49:32 -0500

    Major refactoring to search js, bring marker style in line with result box

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -6,11 +6,21 @@ var contact_path = "/fb_contacts";
 var delayLoad = "<%= @delayLoad %>";*/
 
 var map;
-var markers = [];
-var open_windows = [];
-var index = 0;
+
+var jobs = [];
+var job_markers = [];
+
 var contacts = [];
-var cmarkers = [];
+var contact_markers = [];
+
+var open_windows = [];
+
+$('div#header h1 a').click(function(evt) {
+  if (window != window.top) {
+    evt.preventDefault();
+    window.top.history.back();
+  }
+});
 
 function loadMap() {
   var center = new google.maps.LatLng(39.109, -94.589);
@@ -39,100 +49,76 @@ function loadMap() {
   }
 }
 
-function loadResults(onSucess, onFailure) {
+function loadResults(onSuccess, onFailure) {
   $.ajax({
     url : query_path,
     type : 'get',
     data : {'q' : q, 'l' : l},
-    
     success : function(results, req, status) {
-      var bounds = new google.maps.LatLngBounds();
-      for (var i = 0; i < results.length; i++) {
-        job = results[i].job;
-        
-        if (job.lat == null || job.long == null) {
-          continue;
-        }
-        
-        var marker = new google.maps.Marker({
-          map: map,
-          animation: google.maps.Animation.DROP,
-          position: new google.maps.LatLng(job.lat, job.long),
-          title: job.title + ' - ' + job.company 
-        });
-        
-        addToResultsList(job, i);
-        
-        createJobInfoWindow(job, marker, map);
-        
-        bounds.extend(marker.position);
-        
-        markers.push(marker);
-      }
-      
-      $('div#results_list ul#results li').mouseenter(function(evt) {
-        var i = evt.target.getAttribute("data-index");
-        markers[i].setIcon("/images/marker-blue.png");
-      });
-      
-      $('div#results_list ul#results li').mouseleave(function(evt) {
-        var i = evt.target.getAttribute("data-index");
-        markers[i].setIcon("/images/marker-red.png");
-      });
-      
+      handleResults(results, req, status);
       if (results.length > 0) {
-        map.panToBounds(bounds);
-        map.fitBounds(bounds);
-        
-        var again_text = 'You searched for ' + q +
-              ' jobs';
-        if (l != '') {
-          again_text += ' near ' + l;
-        }
-        again_text += '.&nbsp;&nbsp;<a href="/">Search again?</a>';
-        
-        $('div#search_again').html(again_text);
-        $('div#search_again').css({display: 'block'});
-        $('div#search_again a').click(function(evt) {
-          evt.preventDefault();
-          $('div#search_again').html(
-            '<form id="search" method="get">'+
-                '<input name="q" type="text" placeholder="'+q+'">' +
-                'near <input name="q" type="text" placeholder="'+l+'">' +
-                '<input type="submit" value="go!">' +
-            '</form>');
-        });
-        
-        onSucess();
+        if (onSuccess)
+          onSuccess();
       } else {
-        onFailure();
+        if (onFailure)
+          onFailure();
       }
     }
   });
 }
 
-function addToResultsList(job, index) {
-  $('div#results_list ul#results').append(
-    "<li data-index=\"" + index + "\">" +
-      "<p class=result_title><a href=\""+ job.source + "\">" +
-        job.title + "&mdash;" + job.company + ", " +
-        job.city + ", " + job.state + "</a></p>" + 
-      "<p class=result_text>" + job.desc + "</p>" +
-    "</li>");
+function handleResults(results, req, status) {
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < results.length; i++) {
+    job = results[i].job;
+    
+    if (job.lat == null || job.long == null) {
+      continue;
+    }
+    
+    var marker = addResult(job, i);
+    bounds.extend(marker.position);
+    job_markers.push(marker);
+  }
+  
+  setupResultsBox();
+  createNewSearchBox();
+  
+  if (results.length > 0) {
+    map.panToBounds(bounds);
+    map.fitBounds(bounds);
+  }
 }
 
-function createJobInfoWindow(job, marker, map) {
-  var contentString = '<div class="job_infowindow"><p><strong><a href="'+job.source+'">' + 
-      job.title+'</a></strong><br />'+job.company+'<br />'+
-      job.desc+'<br /><em>'+job.city+', '+job.state+'</em>'+' '+job.date+"</p></div>";
+function addResult(job, index) {
+  var innerText = 
+    "<p class=result_title><a href=\""+ job.source + "\" target='window"+index+"'>" +
+      job.title + "&mdash;" + job.company + ", " +
+      job.city + ", " + job.state + "</a></p>" + 
+    "<p class=result_text>" + job.desc + "</p>";
+  
+  $('div#results_list ul#results').append(
+    "<li data-index=\"" + index + "\">" +
+      innerText +
+    "</li>");
+  
+  var marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: new google.maps.LatLng(job.lat, job.long),
+    title: job.title + ' - ' + job.company 
+  });
   
   marker.infowindow = new google.maps.InfoWindow({
-    content: contentString,
     zIndex: 3,
     maxWidth: 375,
   });
   
-
+  marker.infowindow.setContent(
+    "<div class='job_marker'>" +
+      innerText +
+    "</div>");
+  
   google.maps.event.addListener(marker, 'click', function() {
     while (open_windows.length > 0) {
       open_windows.pop().close();
@@ -140,13 +126,22 @@ function createJobInfoWindow(job, marker, map) {
     open_windows.push(marker.infowindow);
     marker.infowindow.open(map, marker);
   });
+  
+  return marker;
 }
 
 function clearResults() {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
+  for (var i = 0; i < job_markers.length; i++) {
+    job_markers[i].setMap(null);
   }
-  markers = [];
+  job_markers = [];
+  
+  for (var i = 0; i < contact_markers.length; i++) {
+    contact_markers[i].setMap(null);
+  }
+  contact_markers = [];
+  
+  $('div#results_list ul').html('');
 }
 
 function loadContacts() {
@@ -154,40 +149,18 @@ function loadContacts() {
     url: contact_path,
     type: 'get',
     data: {},
-  
+    
     success : function(results, req, status) {
       for (var i = 0; i < results.length; i++) {
-        var c = results[i].facebook_contact;
-        if (c.location && !(c.location == '')) {
-          contacts.push(results[i].facebook_contact);      
-        }
+        contacts.push(results[i]);
+        
       }
-      syncGeocoding(contacts[index]);
     }
   });
 }
 
-function syncGeocoding(c) {
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode( {'address' : c.location}, function(latlngs, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var marker = new google.maps.Marker({
-        map: map,
-        animation: google.maps.Animation.DROP,
-        position: latlngs[0].geometry.location,
-        title: c.name,
-      });
-      createContactInfoWindow(c, marker, map);
-      cmarkers.push(marker);
-    } else {
-      setTimeout(function() {syncGeocoding(c)}, 500);
-    }
-    if (index < contacts.length-1) {
-      setTimeout(function() {      
-        syncGeocoding(contacts[++index]);
-      }, 500);
-    }
-  });    
+function loadContact(id) {
+  
 }
 
 function createContactInfoWindow(c, marker, map) {
@@ -205,5 +178,38 @@ function createContactInfoWindow(c, marker, map) {
     }
     open_windows.push(marker.infowindow);
     marker.infowindow.open(map, marker);
+  });
+}
+
+function setupResultsBox() {
+  $('div#results_list ul#results li').hover(function(evt) {
+    var i = evt.target.getAttribute("data-index");
+    job_markers[i].setIcon("/images/marker-blue.png");
+  },
+  
+  function(evt) {
+    var i = evt.target.getAttribute("data-index");
+    job_markers[i].setIcon("/images/marker-red.png");
+  });
+}
+
+function createNewSearchBox() {
+  var again_text = 'You searched for ' + q +
+        ' jobs';
+  if (l != '') {
+    again_text += ' near ' + l;
+  }
+  again_text += '.&nbsp;&nbsp;<a href="/">Search again?</a>';
+  
+  $('div#search_again').html(again_text);
+  $('div#search_again').css({display: 'block'});
+  $('div#search_again a').click(function(evt) {
+    evt.preventDefault();
+    $('div#search_again').html(
+      '<form id="search" method="get">'+
+          '<input name="q" type="text" placeholder="'+q+'">' +
+          'near <input name="q" type="text" placeholder="'+l+'">' +
+          '<input type="submit" value="go!">' +
+      '</form>');
   });
 }
